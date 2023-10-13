174c19bb0f6b7c7d8d5480349ae3b809cbe8cb7f
Mario Jasso <heximario@gmail.com>
2023-10-13 15:07:23 -0600

    new project vue

---
+++ proyecto/src/views/ImagesView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="images">
+        <h1 class="text-center">Imagenes</h1>
+        
+          
+    </div>
+</template>
+<script>
+export default {
+    name: "ImagesView",
+    
+}
+</script>
